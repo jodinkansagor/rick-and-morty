c12b4e7d4347f99bfc70d9a5644b4995b2405c8e
Jodi N Kansagor <joedvice@gmail.com>
2020-01-28 13:31:17 -0800

    initial character class component build

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Character from './Character.js';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Character />;
 }
-  
---
+++ src/components/Character.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Character extends Component {
+
+  static propTypes = {
+    image: PropTypes.string.isRequired,
+    idNumber: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired,
+    gender: PropTypes.string.isRequired,
+    origin: PropTypes.string.isRequired,
+    lastLocation: PropTypes.string.isRequired
+  }
+
+  render() {
+    const { image, idNumber, status, species, gender, origin, lastLocation } = this.props; 
+    return (
+      <>
+        <image>{image}</image>
+        <p>id: {idNumber} - created 2 years ago</p>
+        <div className="line"><p>STATUS</p><p>{status}</p></div>
+        <div className="line"><p>SPECIES</p><p>{species}</p></div>
+        <div className="line"><p>GENDER</p><p>{gender}</p></div>
+        <div className="line"><p>ORIGIN</p><p>{origin}</p></div>
+        <div className="line"><p>LAST 
+          LOCATION</p><p>{lastLocation}</p></div>
+       
+      </>
+    );
+  }
+};
