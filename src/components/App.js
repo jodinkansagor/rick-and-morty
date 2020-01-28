8a2429ec5d4ab878846d636acc8024f3dcad68aa
Jodi N Kansagor <joedvice@gmail.com>
2020-01-28 15:40:58 -0800

    working on styles for the characters

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,24 @@
 import React from 'react';
-import styles from './App.css';
 import Character from './Character.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import styles from './App.css';
 
-// const Header = () => (
-//   <header className={styles.Header}>
-//     <nav>
-//       <ul>
-//         <li><a className={styles.active} href="#">Home</a></li>
-//         <li><a href="#">About</a></li>
-//         <li><a href="#">Documentation</a></li>
-//       </ul>
-//     </nav>
-//     <section>
-//       <h1>The Rick And Morty Api</h1>
-//       <h2>Hey, did you ever want to hold a Terry fold?</h2>
-//     </section>
-//   </header>
-// );
 
-// const Footer = () => (
-//   <footer className={styles.Footer}>
-//     <section className={styles.stats}>
-//       <p>Characters: 493</p>
-//       <p>Locations: 76</p>
-//       <p>Episodes: 31</p>
-//     </section>
-//     <section className={styles.acknowledgment}>
-//       <p>❮❯ by <a href="#">Axel Fuhrmann</a> 2020</p>
-//     </section>
-//   </footer>
-// );
 
 const App = () => (
   <>
     <Header />
-    <Character image="../../assets/82,jpg" name="Cronenberg Rick" idNumber={82} status="unknown" species="Cronenberg" gender="Male" origin="Cronenberg Earth" lastLocation="Earth(C-137)" />
-    <Character image="../../assets/158.jpg" name="Hookah Alien" idNumber={158} status="Alive" species="Alien, Tuskfish" gender="unknown" origin="unknown" lastLocation="Interdimentional Customs" />
-    <Character image="../../assets/172.jpg" name="Jamey" idNumber={172} status="Alive" species="Human" gender="Male" origin="Earth (Replacement Dimension" lastLocation="Earth (Replacement Dimension" />
-    <Character image="../../assets/220.jpg" name="Mega Fruit Farmer Rick" idNumber={220} status="Alive" species="Human" gender="Male" origin="unkown" lastLocation="Citadel of Rocks" />
-    <Character image="../../assets/241.jpg" name="Mr. Marklovitz" idNumber={214} status="Alive" species="Human" gender="Male" origin="Earth (C-137)" lastLocation="Earth (Replacement Dimension)" />
-    <Character image="../../assets/259.jpg" name="Pencilvester" idNumber={259} status="Dead" species="Alien, Parasite, Pencil" gender="Male" origin="unknown" lastLocation="Earth (Replacement Dimension)" />
-    <Character image="../../assets/335.jpg" name="Steve" idNumber={335} status="Alive" species="Human" gender="Male" origin="Earth (Replacement Dimension)" lastLocation="Earth (Replacement Dimension)" />
-    <Character image="../../assets/387.jpg" name="Zarbadar's Mytholog" idNumber={387} status="unkown" species="Mytholog" gender="Female" origin="Nuptia 4" lastLocation="Nuptia 4" />;
+    <div className={styles.bigDiv}>
+      <Character image="../../assets/82.jpeg" name="Cronenberg Rick" idNumber={82} status="unknown" species="Cronenberg" gender="Male" origin="Cronenberg Earth" lastLocation="Earth(C-137)" />
+      <Character image="../../assets/158.jpeg" name="Hookah Alien" idNumber={158} status="Alive" species="Alien, Tuskfish" gender="unknown" origin="unknown" lastLocation="Interdimentional Customs" />
+      <Character image="../../assets/172.jpeg" name="Jamey" idNumber={172} status="Alive" species="Human" gender="Male" origin="Earth (Replacement Dimension" lastLocation="Earth (Replacement Dimension" />
+      <Character image="../../assets/220.jpeg" name="Mega Fruit Farmer Rick" idNumber={220} status="Alive" species="Human" gender="Male" origin="unkown" lastLocation="Citadel of Rocks" />
+      <Character image="../../assets/241.jpeg" name="Mr. Marklovitz" idNumber={214} status="Alive" species="Human" gender="Male" origin="Earth (C-137)" lastLocation="Earth (Replacement Dimension)" />
+      <Character image="../../assets/259.jpeg" name="Pencilvester" idNumber={259} status="Dead" species="Alien, Parasite, Pencil" gender="Male" origin="unknown" lastLocation="Earth (Replacement Dimension)" />
+      <Character image="../../assets/335.jpeg" name="Steve" idNumber={335} status="Alive" species="Human" gender="Male" origin="Earth (Replacement Dimension)" lastLocation="Earth (Replacement Dimension)" />
+      <Character image="../../assets/387.jpeg" name="Zarbadar's Mytholog" idNumber={387} status="unkown" species="Mytholog" gender="Female" origin="Nuptia 4" lastLocation="Nuptia 4" />;
+    </div>
     <Footer />
   </>
 );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './App.css';
+
 
 export default class Character extends Component {
 
@@ -17,18 +19,17 @@ export default class Character extends Component {
   render() {
     const { image, name, idNumber, status, species, gender, origin, lastLocation } = this.props; 
     return (
-      <>
-        <img src = {image} />
+      <section className={styles.characterSection}>
+        <img className={styles.characterImage} src={image} />
         <p>{name}</p>
         <p>id: {idNumber} - created 2 years ago</p>
-        <div className="line"><p>STATUS</p><p>{status}</p></div>
-        <div className="line"><p>SPECIES</p><p>{species}</p></div>
-        <div className="line"><p>GENDER</p><p>{gender}</p></div>
-        <div className="line"><p>ORIGIN</p><p>{origin}</p></div>
-        <div className="line"><p>LAST 
+        <div className={styles.line}><p>STATUS</p><p>{status}</p></div>
+        <div className={styles.line}><p>SPECIES</p><p>{species}</p></div>
+        <div className={styles.line}><p>GENDER</p><p>{gender}</p></div>
+        <div className={styles.line}><p>ORIGIN</p><p>{origin}</p></div>
+        <div className={styles.line}><p>LAST 
           LOCATION</p><p>{lastLocation}</p></div>
-       
-      </>
+      </section>
     );
   }
-};
+}
